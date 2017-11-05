90dc7134022a0c54285f7c0fa8667f0370c860db
yjosh03 <33265167+yjosh03@users.noreply.github.com>
2017-11-05 13:31:07 -0500

    Update leafletChoropleth.js

--- src/js/leafletChoropleth.js
+++ src/js/leafletChoropleth.js
@@ -1,1 +1,12 @@
+// Set map var
+var coords = [37.8,-96]; // set center point var
+var zoom = 5; // set zoom level var
+var map = L.map('map').setView(coords, zoom); // put it all together
+
+////////////////////////////////////////////////////////////////////
+// load a basemap tie layer
+L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Data by <a href="https://cdflint.github.io/">Carl Flint</a>'
+}).addTo(map);
 
